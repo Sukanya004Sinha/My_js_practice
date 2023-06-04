342c0ab3f3deee4453aec7264c0919aa510bd341
sukanya004sinha <sukanyasinha004@gmail.com>
2023-06-04 18:05:03 +0530

    javascript revision

---
+++ index.js
@@ -0,0 +1,34 @@
+var myName = 'Sukanya';
+// var myAge = 24;
+// console.log(myAge);
+// var _myName = "sukanya";
+// var _1my__Name = "sinha";
+// var 1myName = "ss";
+// var $myName = "ssss"
+// var myName% = "sss"
+// console.log(myName%);
+var isSukanya = false;
+var myAge = 24;
+//console.log(typeOf(myAge));
+console.log(isSukanya);
+console.log(typeof(isSukanya));
+
+console.log(10+"20"); //1020
+console.log(9-5); //4
+console.log(9-"5"); //bug
+console.log("java"+"script"); //javascript
+console.log(" "+" "); // space empty
+console.log(" "+0); //0
+console.log ("sukanya"- "sinha"); //NaN
+console.log(true+false); // 1 means true
+console.log(false-true);  // -1
+console.log(true-false); // 1 
+console.log(false+false); //0
+
+
+
+
+
+
+
+
---
+++ index1.js
@@ -0,0 +1,8 @@
+//Difference between undefined and Null
+
+var sukanya = null
+console.log(typeof(null));
+console.log(sukanya);
+
+var ss;
+console.log(ss);
